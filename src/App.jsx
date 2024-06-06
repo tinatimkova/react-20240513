8a1019f3ff63cd29ec4e7bc66a4c0606247dbcd4
timosha <tinaatimkovaa@gmail.com>
2024-06-06 02:00:02 -0400

    configured store and created slices

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import { Restaurants } from './components/restaurants/component.jsx';
 import { Layout } from './components/layout/component.jsx';
 import { ThemeContextProvider } from './contexts/theme/provider.jsx';
 import { UserContextProvider } from './contexts/user/provider.jsx';
+import { Provider } from 'react-redux';
+import { store } from './redux/index.js'; 
 
 export const App = () => {
     return (
+        <Provider store={store}>
         <ThemeContextProvider>
             <UserContextProvider>
                 <Layout>
@@ -13,5 +16,6 @@ export const App = () => {
                 </Layout>
             </UserContextProvider>
         </ThemeContextProvider>
+        </Provider>
     );
 };
---
+++ src/redux/index.js
@@ -0,0 +1,11 @@
+import { combineSlices, configureStore } from "@reduxjs/toolkit";
+import { RestaurantSlice } from "./entities/restaurant";
+import { UserSlice } from "./entities/user";
+import { ReviewSlice } from "./entities/review";
+import { DishSlice } from "./entities/dish";
+
+export const store = configureStore({
+    reducer: combineSlices(RestaurantSlice, UserSlice, ReviewSlice, DishSlice), 
+});
+
+console.log(store.getState());
